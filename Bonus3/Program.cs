4ddc1a8df0aaf25ae865ba56e50bbfdec006fc78
William Chapman <chapman.williamt@gmail.com>
2018-07-17 15:14:22 -0400

    init commit

---
+++ Bonus3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bonus3
+{
+    class Program
+    {
+        static void Main()
+        {
+            //generate random number
+            //get 1-100 int from user 
+            //more than ten too high "Way too high"
+            //more than ten too low "Way too low"
+            //less than ten too high "Too high"
+            //less than ten too low "Too low"
+            //win message should be different based on how many guesses it takes them
+            //only accept numbers 1-100
+
+            //y/n continue
+            //if invalid continue data, prompt again
+            //separate methods
+        }
+    }
+}
